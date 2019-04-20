a25d6478cb08922f3bb7656069aab4228d7ed752
sivaEkumaran <siva98kumarane@gmail.com>
2019-04-20 08:53:43 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int t,a,b;
+    printf("Type Two numbers\n");
+    printf("Type no 1:");
+    scanf("%d",&a);
+    printf("Type no 1:");
+    scanf("%d",&a);
+    t=a+b;
+    printf("Total=%d",t);
+}
